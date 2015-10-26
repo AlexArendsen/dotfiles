2ad1255e7effae2538d8406b0f22a32727f7812b
Alex Arendsen <alex.arendsen.7@gmail.com>
2015-10-26 11:23:15 -0400

    Added systemd wallpaper units

--- sync.sh
+++ sync.sh
@@ -8,15 +8,21 @@ mkdir -p dots
 mkdir -p dots/.i3
 
 sudo cp ~/sbin/* util/
+
 cp ~/.Xresources dots/
-cp ~/.i3/config dots/.i3
-cp -r ~/.i3/lemonbar dots/.i3
-cp ~/.config/compton/compton.conf dots/
 cp ~/.tmux.conf dots/
 cp ~/.vimrc dots/
 cp ~/.vromerc dots/
 cp ~/.bashrc dots/
 cp ~/.zshrc dots/
 cp ~/.xinitrc dots/
+
+cp ~/.i3/config dots/.i3
+cp -r ~/.i3/lemonbar dots/.i3
+
+cp ~/.config/compton/compton.conf dots/
 cp ~/.config/xfce4/terminal/terminalrc dots/
 cp ~/.config/sublime-text-3/Packages/User/ApplyXres.template dots/
+
+sudo cp /etc/systemd/system/bkg.service dots/
+sudo cp /etc/systemd/system/bkg.timer dots/
